6b5880bfc974b4a5a0c6227f8a692f8a4c0a4fdf
Hope Gipson <gipson.hope@gmail.com>
2020-06-24 01:18:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ array.each_with_index do |names, index|
       counter_number = index + 1
         array[names].prepend "#{counter_number}. "
         puts "The line is currently: " + array.join
-
+end
 end
 
 end
