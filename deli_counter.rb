692f11f1030f1bd12f7d40a427642aac443b669e
Hope Gipson <gipson.hope@gmail.com>
2020-06-24 01:09:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,13 @@ def take_a_number(array, name)
   end
 
 def now_serving(array)
-  
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+  first = array.shift
+  puts "Currently serving #{first}."
+end
+end
 
       
 
