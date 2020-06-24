71ecbf220b2ccba4aec627f40263de348dea35bd
Hope Gipson <gipson.hope@gmail.com>
2020-06-24 00:26:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,8 @@
 # Write your code here.
 def line(array)
-  if array.length == 0
+while array.length == 0
     puts "The line is currently empty."
-else
-  puts "nil"
-  end
+end
 end
 
 def take_a_number(array, name)
