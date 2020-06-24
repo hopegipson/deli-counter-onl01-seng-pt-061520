2e831ffac95cb2371d58081361a5ef096fd787bd
Hope Gipson <gipson.hope@gmail.com>
2020-06-24 00:52:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,9 @@ if array.length == 0
     puts "The line is currently empty."
 
 else
-  map.
-end
+array.each_with_index do |names, index|
+    room_number = index + 1
+    room_assignment = "Hello, #{names}! You'll be assigned to room #{room_number}!"end
 end
 
 def take_a_number(array, name)
